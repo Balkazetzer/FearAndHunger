602d3b37f9cdc3d84895d89d4c80fdf2ac68478a
Balkazetzer <143459548+Balkazetzer@users.noreply.github.com>
2023-11-24 01:36:03 -0600

    ACTUALIZACION DE LA CLASE VENTANA

--- include/Nivel.hpp
+++ include/Nivel.hpp
@@ -3,15 +3,40 @@
 #include <stdio.h>
 #include <string>
 #include "Ventana.hpp"
+#include "Globales.hpp"
 
 using namespace std;
+struct SDL_Texture;
 
 class Nivel
 {
 private:
-    /* data */
+    string _nombreMapa;
+    Vector2 _puntoSpawn;
+    Vector2 _tamanyo;
+
+    SDL_Texture* _texturaFondo;
+    void CargarMapa(string nombreMapa, Ventana &ventana);
+
 public:
-    Nivel(string Mapname, Ventana &Ventana);
+    Nivel(string nombreMapa, Vector2 puntoSpawn, Ventana &Ventana);
     ~Nivel();
+
+    void ActualizarMapa(int lapso);
+    void Dibujar(Ventana &ventana);
 };
 
+Nivel::Nivel(string nombreMapa, Vector2 puntoSpawn, Ventana &Ventana):
+
+_nombreMapa(nombreMapa), 
+_puntoSpawn(puntoSpawn),
+_tamanyo(Vector2(0,0))
+
+{
+    this->CargarMapa(nombreMapa, Ventana);
+}
+
+void Nivel::CargarMapa(string nombreMapa, Ventana &ventana){
+    this->_texturaFondo = SDL_CreateTextureFromSurface(ventana.GetRenderer)
+
+}
--- include/Ventana.hpp
+++ include/Ventana.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <string>
+#include <map>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
 
 class Ventana
 {
@@ -11,20 +16,60 @@ protected:
 
     SDL_Window *mainVentana = NULL;      // Ventana de contenido
     SDL_Renderer *mainSuperficie = NULL; // Contenido base
+    map<string, SDL_Surface *> _sprites;
 
 public:
     Ventana(/* args */);
     ~Ventana();
+
+    SDL_Surface *cargarImagen(const string &path);
+    void blitSuperficie(SDL_Texture *fuente, SDL_Rect *rectFuente, SDL_Rect *rectDestino);
+    void flip();
+    void clear();
+
+    SDL_Renderer *GetRenderer() const;
 };
 
+/*-----------------DEFINICION DE LAS FUNCIONES-------------*/
+
 // CONSTRUCTOR Y DESTRUCTOR
-Ventana::Ventana(/* args */){
-    SDL_CreateWindowAndRenderer(SCREEN_WIDTH,SCREEN_HEIGHT, 0, &this->mainVentana, &this->mainSuperficie);
+Ventana::Ventana(/* args */)
+{
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &this->mainVentana, &this->mainSuperficie);
     SDL_SetWindowTitle(this->mainVentana, "Fear and Hunger");
-    }
+}
 
 Ventana::~Ventana()
 {
     SDL_DestroyWindow(this->mainVentana);
-    
+}
+
+SDL_Surface *Ventana ::cargarImagen(const string &path)
+{
+    if (this->_sprites.count(path) == 0)
+    {
+        this->_sprites[path] = IMG_Load(path.c_str());
+    }
+    return this->_sprites[path];
+}
+
+void Ventana ::blitSuperficie(SDL_Texture *textura, SDL_Rect *rectFuente, SDL_Rect *rectDestino)
+{
+    SDL_RenderCopy(this->mainSuperficie, textura, rectFuente, rectDestino);
+}
+
+void Ventana ::flip()
+{
+    SDL_RenderPresent(this->mainSuperficie);
+}
+
+void Ventana::clear()
+{
+    SDL_RenderClear(this->mainSuperficie);
+}
+
+SDL_Renderer *Ventana ::GetRenderer() const
+{
+
+    return this->mainSuperficie;
 }
