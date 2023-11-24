6d102fd4dc613f15dbe3a2b36840997020f888a6
Balkazetzer <143459548+Balkazetzer@users.noreply.github.com>
2023-11-23 23:46:51 -0600

    Añadidura del header "Globales"

---
+++ include/Globales.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+using namespace std;
+
+namespace globales{
+
+    const int ALTURA_VENTANA = 480;
+    const int ANCHO_VENTANA = 640;
+
+    const float ESCALA_SPRIDE = 2.0f;
+}
+
+struct Vector2{
+    int x, y;
+    Vector2():
+    x(0), y(0)
+    {}
+    Vector2(int x, int y):
+    x(x), y(y)
+    {}
+    Vector2 zero(){
+        return Vector2(0, 0);
+    }
+
+};
